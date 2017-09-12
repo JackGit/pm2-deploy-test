1dcb4fd7f36672361b6ad3dd8047104617e54452
Jack Yang <ygjack414@hotmail.com>
2017-09-12 18:36:57 +0800

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'API',
+      script    : 'app.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+
+    // Second application
+    {
+      name      : 'WEB',
+      script    : 'web.js'
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'jack',
+      host : 'jackyang.me',
+      ref  : 'origin/master',
+      repo : 'git@github.com:jackgit/pm2-deploy-test.git',
+      path : '/home/jack/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'jack',
+      host : 'jackyang.me',
+      ref  : 'origin/master',
+      repo : 'git@github.com:jackgit/pm2-deploy-test.git',
+      path : '/home/jack/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
